548cabe754a501eb0a167e57a8775782d9032ecc
Vengage <vengage_92@yahoo.com>
2014-11-10 10:00:22 +0200

    First commit

---
+++ src/com/tema5/Main.java
@@ -0,0 +1,14 @@
+package com.tema5;
+
+
+/**
+ * Created by Cosovanu Vasile 10.11.2014
+ *
+ * This is the main class for Tema 5
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
